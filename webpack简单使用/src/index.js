a11585f6b076b21abf895f8ff05ebaa8ccaf2ce6
GHjiejie <3426571530@qq.com>
2024-05-27 00:57:16 +0800

    webpack-day1

---
+++ webpack简单使用/src/index.js
@@ -0,0 +1,7 @@
+// 导入css样式文件
+import "./public/css/index.css";
+import "./public/css/index.scss";
+function Hello() {
+  console.log("Hello Webpack!");
+}
+Hello();
---
+++ webpack简单使用/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    // 设置资源模块的输出路径，通常不这样设置，因为这里只有一个资源模块，所以设置为images,需要考虑到后面还有很多不同类型的资源模块
+    // assetModuleFilename: "images/[hash][ext][query]",
+    // clean: true, //这个方法调用的是底层的fs.rm()方法，所以不会有任何文件被移动到系统的垃圾箱，而是直接删除，所以要小心使用（不推荐）
+  },
+
+  // 设置模块
+  module: {
+    rules: [
+      // 处理css文件
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // 处理scss文件
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      // 处理图片文件
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset",
+        // 配置资源文件的解析方式，小于8kb的资源文件会被解析为base64格式，大于8kb的资源文件会被解析为资源模块
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 8kb
+          },
+        },
+        // 设置单独文件资源输出路径
+        generator: {
+          filename: "images/[hash:10][ext][query]",
+        },
+      },
+    ],
+  },
+
+  // 配置插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 设置模板文件，打包时会根据此模板生成html文件，同时引入打包后的js文件
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin({
+      // 这里面可以实现很多功能，比如删除指定文件，删除指定文件夹，删除指定文件夹下的文件，删除指定文件夹下的指定文件等等
+      // cleanOnceBeforeBuildPatterns: ["**/*"],
+    }),
+    // 将css打包成一个css文件
+    new MiniCssExtractPlugin({}),
+  ],
+
+  // 配置开发服务器
+
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    // 开启热更新
+    hot: true,
+    open: true,
+  },
+  // 设置模式
+  mode: "development",
+};
